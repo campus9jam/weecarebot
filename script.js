a5871741c9d11a0642fb356ce87a150a5b68a0e0
ARogo <aminuarogo@gmail.com>
2024-12-15 14:42:43 -0800

    Initial commit of WeCare Telehealth System

---
+++ script.js
@@ -0,0 +1,19 @@
+function bookAppointment() {
+    alert("Booking an appointment...");
+}
+
+function startConsultation() {
+    alert("Starting a consultation...");
+}
+
+function setReminder() {
+    alert("Setting a medication reminder...");
+}
+
+function viewArticles() {
+    alert("Viewing health articles...");
+}
+
+function manageContacts() {
+    alert("Managing emergency contacts...");
+}
